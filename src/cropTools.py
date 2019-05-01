tweaking to fix off by one pixel crop issue

src/cropTools.py
@@ -88,7 +88,16 @@ def cropImagesToAxis(images, offsets, axis):
     return croppedImages[:, 0], croppedImages[:, 1]
 
 def cropImagesToOffsets(images, offsets):
-    images = np.array(images)
+
+    imageDimensions = np.array([image.shape for image in images])
+    minHeight = np.min(imageDimensions[:, 0])
+    minWidth = np.min(imageDimensions[:, 1])
+    print('Min Width :: {}, Min Height :: {}'.format(minHeight, minWidth))
+
+    #Occasionally there is data that is 1 px smaller. Probably a rounding issue on the cropping app side. On todo list...
+    images = np.array([image[0:minHeight, 0:minWidth] for image in images])
+
+    #images = np.array(images)
     offsets = np.array(offsets)
     updatedOffsets = np.copy(offsets)
     images, xOffsets = cropImagesToAxis(images, offsets[:, X], X)

tools/aggregateStats.py
@@ -184,5 +184,6 @@ else:
     valueVsFluxish = np.stack([bestGuesses[:, 2], fluxishes], axis=1)
     print('Values vs Fluxishes :: ' + str(valueVsFluxish))
     plot2d(valueVsFluxish, 'Value', 'Fluxish')
-    plotHist(bestGuesses[:, 2])
+    plotHist(bestGuesses[:, 2]) #Pretty sure the target for this is a distribution with a range of ~0.1? Would pretty accuately place it in the spectrum?
+
 