e44a9122863aaa7130c3499eee3ec588c5b61cfa
Doug MacEwen <d2macewen@gmail.com>
2020-01-13 13:22:49 -0800

    clean up application.py

--- src/application.py
+++ src/application.py
@@ -1,15 +1,21 @@
+"""
+Lightweight server that runs in the Elastic Beanstalk worker
+    The worker environment (where this is running) recieves an SQS message and makes a POST request with the message contents
+    This server calls the image processing pipeline with the arguments
+"""
 from wsgiref.simple_server import make_server
-from runSteps import run
 import json
+from runSteps import run
 
 from logger import getLogger
-logger = getLogger(__name__, 'server')
+LOGGER = getLogger(__name__, 'server')
 
 isProduction = __name__ != '__main__'
 
 def application(environ, start_response):
-    path    = environ['PATH_INFO']
-    method  = environ['REQUEST_METHOD']
+    """Worker environment looks for this function (named application) to set up the server"""
+    path = environ['PATH_INFO']
+    method = environ['REQUEST_METHOD']
     if method == 'POST':
         try:
             if path == '/':
@@ -22,31 +28,32 @@ def application(environ, start_response):
                 capture_id_key = 'capture_id'
 
                 if user_id_key not in requestForm:
-                    logger.warning('No user_id')
+                    LOGGER.warning('No user_id')
                 user_id = requestForm[user_id_key]
 
                 if session_id_key not in requestForm:
-                    logger.warning('No session_id')
+                    LOGGER.warning('No session_id')
                 session_id = requestForm[session_id_key]
 
                 if capture_id_key not in requestForm:
-                    logger.warning('No capture_id')
+                    LOGGER.warning('No capture_id')
                 capture_id = requestForm[capture_id_key]
-                
-                logger.info('PROCESSING :: user_id :: {}, session_id :: {}, capture_id :: {}'.format(user_id, session_id, capture_id))
+
+                LOGGER.info('PROCESSING :: user_id :: %s, session_id :: %s, capture_id :: %s', user_id, session_id, capture_id)
                 isSuccessful = run(user_id, capture_id, isProduction)['successful']
+
                 if isSuccessful:
-                    logger.info('IS SUCCESSFUL :: user_id :: {}, session_id :: {}, capture_id :: {}'.format(user_id, session_id, capture_id))
+                    LOGGER.info('IS SUCCESSFUL :: user_id :: %s, session_id :: %s, capture_id :: %s', user_id, session_id, capture_id)
                 else:
-                    logger.warning('NOT SUCCESSFUL :: user_id :: {}, session_id :: {}, capture_id :: {}'.format(user_id, session_id, capture_id))
+                    LOGGER.warning('NOT SUCCESSFUL :: user_id :: %s, session_id :: %s, capture_id :: %s', user_id, session_id, capture_id)
 
                 response = str(isSuccessful)
 
             elif path == '/scheduled':
-                logger.info("Received task %s scheduled at %s", environ['HTTP_X_AWS_SQSD_TASKNAME'], environ['HTTP_X_AWS_SQSD_SCHEDULED_AT'])
+                LOGGER.info("Received task %s scheduled at %s", environ['HTTP_X_AWS_SQSD_TASKNAME'], environ['HTTP_X_AWS_SQSD_SCHEDULED_AT'])
                 response = str(True)
         except Exception as e:
-            logger.warning('Error retrieving request body for async work. {}'.format(e))
+            LOGGER.warning('Error retrieving request body for async work. %s', e)
             response = str(False)
     else:
         response = 'Probably not what you are looking for...'
@@ -61,4 +68,3 @@ if not isProduction:
     httpd = make_server('', 8000, application)
     print("Serving on port 8000...")
     httpd.serve_forever()
-
